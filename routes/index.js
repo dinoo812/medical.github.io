6693ef8e4f8ab0d2e1dc07691be42c06919b9c89
dinoo812 <dinoo812@gmail.com>
2017-05-10 12:06:15 +0530

    All

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Medical' });
+  res.render('index', { title: 'PR Medical' });
 });
 
 module.exports = router;
